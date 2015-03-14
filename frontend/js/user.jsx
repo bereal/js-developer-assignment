0e08dad7dd0ba0de350a110b992d1398ef416373
Alexander Berezhnoy <alexander.berezhnoy@gmail.com>
2015-03-15 00:35:21 +0200

    frontend: Change markup to a table

--- frontend/js/user.jsx
+++ frontend/js/user.jsx
@@ -3,30 +3,10 @@ var _ = require('lodash');
 var classNames = require('classnames');
 var connect = require('./userModel').connect;
 
-var User = React.createClass({
-    getInitialState: function() {
-        return this.props.data || {username: '', roles: []};
-    },
-
-    render: function() {
-        var user = this.props.data;
-        var classes = classNames({
-            'admin': _.contains(user.roles, 'admin'),
-        });
-        return (
-            <div className={classes}>
-                {user.username}
-            </div>
-        );
-    }
-});
-
-
-var ENTER_KEY = 13;
 
 var NewUser = React.createClass({
     getInitialState: function() {
-        return { isAdmin: false };
+        return { };
     },
 
     submit: function() {
@@ -67,19 +47,19 @@ var NewUser = React.createClass({
     },
 
     render: function() {
-        return (
-            <div className="new">
-                <input name="username" ref="username"
-                       onKeyDown={this.handleKeyDown} onChange={this.handleUsername}/>
+        var title = "Fill username and password and press Enter to add a user";
+        return (<div>
+            <input name="username" ref="username" placeholder="Username" title={title}
+                   onKeyDown={this.handleKeyDown} onChange={this.handleUsername}/>
 
-                <input name="password" type="password" ref="password"
-                       onKeyDown={this.handleKeyDown} onChange={this.handlePassword}/>
+            <input name="password" type="password" ref="password" 
+                   placeholder="Password" title={title}
+                   onKeyDown={this.handleKeyDown} onChange={this.handlePassword}/>
 
-                <input type="checkbox" name="isAdmin" ref="newAdmin"
-                       onChange={this.handleCheckbox}/>
-                <label for={this.refs.newAdmin}/>
-            </div>
-        );
+            <input type="checkbox" name="isAdmin" ref="newAdmin"
+                   onChange={this.handleCheckbox}/>
+            <label for={this.refs.newAdmin}>Admin</label>
+       </div>);
     }
 });
 
@@ -102,17 +82,30 @@ var UserList = module.exports.UserList = React.createClass({
     },
 
     render: function() {
+        var header = (
+            <thead>
+                <tr><th>#</th><th>Username</th><th>Roles</th></tr>
+            </thead>
+        );
         var users = this.state.users.map(function(user) {
-            return (<li><User data={user}/></li>);
+            return (
+                <tr>
+                  <td width="5%">{user.id}</td>
+                  <td>{user.username}</td>
+                  <td>{user.roles.join(', ')}</td>
+                </tr>
+            );
         });
         users.push(
-          <li>
+          <tr>
+          <td colSpan="3">
             <NewUser ref="newUser"
                      model={this.props.model}
                      onAddUser={this.addUser}
                      onError={this.props.onError || alert}/>
-          </li>);
-        return (<ul>{users}</ul>);
+          </td>
+          </tr>);
+        return (<table>{header}<tbody>{users}</tbody></table>);
     }
 });
 
