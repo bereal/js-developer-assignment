64a11cdaa6ac3f3bc3dc9dbb989b6b77aa19e15b
Alexander Berezhnoy <alexander.berezhnoy@gmail.com>
2015-03-15 09:00:29 +0200

    backend: Set actor to an empty models.User

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,19 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var serverbone = require('serverbone');
 var collections = require('./collections');
+var models = require('./models');
 var config = require('./config');
 
 var usersResource = new serverbone.resources.Resource('users', {
   collection: collections.Users
 });
 
-var default_actor = {
-  get: function(name) {
-      return name === 'roles' ? [] : null;
-  }
-};
-
 var app = express();
 app.use('/ui', express.static(__dirname + '/../frontend/build'));
 app.use(bodyParser());
 app.use(function (req, res, next) {
-    req.actor = default_actor; next();
+    req.actor = new models.User();
+    next();
 });
 app.use('/users', usersResource.app);
 
